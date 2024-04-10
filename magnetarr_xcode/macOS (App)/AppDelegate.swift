882021ab80bbc93c771a37f2046a1142b983f61e
Vivien Chene <viv.chene@gmail.com>
2024-04-10 21:15:04 +0100

    Add osx/ios extension

---
+++ magnetarr_xcode/macOS (App)/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  macOS (App)
+//
+//  Created by Vivien Chene on 10/04/2024.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
