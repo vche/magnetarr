091cbc416585fceae8f9f300d81f99735f09fe13
Vivien Chene <viv.chene@gmail.com>
2024-04-15 21:13:03 +0100

    Update release to

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+version=$(cat package.json |jq -r '.version')
+echo "--> Updating version to $version"
+
+echo "--> Building packages"
+npm run release
+if [ $? -ne 0 ]; then 
+  echo "The command failed with exit status $?" 
+  exit 1 
+fi
+
+echo "--> Pushing commit"
+igit up -m "Update release to $1"
+if [ $? -ne 0 ]; then 
+  echo "The command failed with exit status $?" 
+  exit 1 
+fi
+
+echo "--> Generating github release v$1"
+gh release create v$1 --generate-notes
+if [ $? -ne 0 ]; then 
+  echo "The command failed with exit status $?" 
+  exit 1 
+fi
